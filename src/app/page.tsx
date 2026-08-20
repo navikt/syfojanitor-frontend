17f355365cfdbee47c1fd1d46303d98ededfc7bd
Daniel Engebretsen <daniel.engebretsen@nav.no>
2026-08-20 10:19:12 +0200

    IS-4199: add SlettBehandlerdialog.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-import LukkDialogmote from "../components/LukkDialogmote";
+import JanitorForms from "../components/JanitorForms";
 import EventStatusListe from "../components/EventStatusListe";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-24 gap-16">
-      <LukkDialogmote/>
+      <JanitorForms />
       <EventStatusListe />
     </main>
   );
---
+++ src/components/JanitorForms.tsx
@@ -0,0 +1,21 @@
+'use client'
+import { Tabs } from "@navikt/ds-react";
+import LukkDialogmote from "./LukkDialogmote";
+import SlettBehandlerdialog from "./SlettBehandlerdialog";
+
+export default function JanitorForms() {
+  return (
+    <Tabs defaultValue="lukk-dialogmote" className="w-full max-w-md">
+      <Tabs.List>
+        <Tabs.Tab value="lukk-dialogmote" label="Lukk dialogmøte" />
+        <Tabs.Tab value="slett-behandlerdialog" label="Slett behandlerdialog" />
+      </Tabs.List>
+      <Tabs.Panel value="lukk-dialogmote" className="pt-6">
+        <LukkDialogmote />
+      </Tabs.Panel>
+      <Tabs.Panel value="slett-behandlerdialog" className="pt-6">
+        <SlettBehandlerdialog />
+      </Tabs.Panel>
+    </Tabs>
+  )
+}
